e28d793d60c8ebd636c63264f67cc19dc3612674
William Huang <william.huang@fazzfinancial.com>
2023-02-20 16:24:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.post('/api/v1/authorization', (req, res) => {
+  res.json({
+    "balances": {
+      "currency_code": "SGD",
+      "ledger_balance": "100.00",
+      "available_balance": "98.00"
+    },
+    "transaction_id": "b0dc7c8d-a346-450f-9b1d-cbd76aa2b0a7",
+    "remarks": "user bought from drink shop"
+  });
+});
+
+app.post('/api/v1/webhook', (req, res) => {
+  res.status(200).send();
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
